68d6f40a2b73ebec2c39c8d55db71a01990fa071
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2016-07-25 18:53:26 -0300

    Testing with any number of generals and traitors

--- tests/customtopo.py
+++ tests/customtopo.py
@@ -6,49 +6,68 @@ from mininet.node import OVSController
 from time import sleep
 
 class CustomTopo(Topo):
-    def __init__(self):
+    def __init__(self, n_generals):
         Topo.__init__(self)
 
-        # criacao dos hosts
-        ne1 = self.addHost('ne1')
-        ne2 = self.addHost('ne2')
-        ne3 = self.addHost('ne3')
-        ne4 = self.addHost('ne4')
-
-        # criacao dos switches
         s1 = self.addSwitch('s1')
 
-        # criacao dos links
-        self.addLink(ne1, s1)
-        self.addLink(ne2, s1)
-        self.addLink(ne3, s1)
-        self.addLink(ne4, s1)
+        # criacao dos hosts e links
+        for i in range(1, n_generals + 1):
+            hostname = "h" + str(i)
+            host = self.addHost(hostname)
 
+            self.addLink(host, s1)
+
+            print hostname + " added"
 
 topos = {'customtopo': (lambda: CustomTopo())}
 
+def decision(i):
+    with open("tmp/gen" + i + ".out", "rb") as f:
+        first = f.readline()      # Read the first line.
+        f.seek(-2, 2)             # Jump to the second last byte.
+        while f.read(1) != b"\n": # Until EOL is found...
+            f.seek(-2, 1)         # ...jump back the read byte plus one more.
+        last = f.readline()       # Read last line.
+        return last
 
-def simpleTest():
-    "Create and test a simple network"
-    topo = CustomTopo()
+def run_environment(n_generals, n_traitors):
+    topo = CustomTopo(n_generals)
     net = Mininet(topo = topo, controller = OVSController)
     net.start()
 
-    net.pingAll()
+    prog_name = "./../out/Debug/byzantine_generals "
 
-    prog = "./../out/Debug/byzantine_generals "
-
-    for i in range(1, 5):
-        hostname = "ne" + str(i)
+    pids = {}
+    for i in range(1, n_generals + 1):
+        hostname = "h" + str(i)
         ne = net.get(hostname)
 
-        general = i % 4
-        ne.cmd(prog + str(general) + " > tmp/gen" + str(general) + ".txt &")
+        print "starting " + hostname
 
-    sleep(6)
+        #Commander has id 0
+        general_id = i % n_generals
+
+        prog_args = str(general_id) + " " + str(n_generals) + " " + str(n_traitors)
+        prog_other = " > tmp/gen" + str(general_id) + ".out &"
+
+        command = prog_name + prog_args + prog_other
+        ne.cmd(command)
+
+        pids[i-1] = int(ne.cmd('echo $!'))
+        sleep(1)
+
+    for i in range(1, n_generals):
+        hostname = "h" + str(i)
+        ne = net.get(hostname)
+        ne.cmd('wait', pids[i-1])
+
     net.stop()
+
+    for i in range(n_traitors + 1, n_generals):
+        print "LT " + str(i) + ": " + decision(str(i))
 
 if __name__ == '__main__':
     # Tell mininet to print useful information
     setLogLevel('info')
-    simpleTest()
+    run_environment(8, 2)
